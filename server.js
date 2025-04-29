15ff29c25a1e5faccc0036b735f9aa5a598de651
Hoàng Phi Hùng <83570379+hung319@users.noreply.github.com>
2025-04-29 10:48:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.post('/upload/text/:filename', (req, res) => {
     const filename = req.params.filename;
     const filePath = path.join(UPLOAD_DIR, filename);
 
+    // Nếu file tồn tại, kiểm tra kết thúc có \n không
     let prefix = '';
     if (fs.existsSync(filePath)) {
         const currentContent = fs.readFileSync(filePath, 'utf8');
@@ -66,7 +67,7 @@ app.get('/text/:filename', (req, res) => {
     }
 });
 
-// GET upload text bằng query
+// GET upload text bằng query (hỗ trợ Onii-chan upload nhanh)
 app.get('/upload/text/:filename/:content', (req, res) => {
     const filename = req.params.filename;
     const content = req.params.content;
@@ -85,7 +86,7 @@ app.get('/upload/text/:filename/:content', (req, res) => {
     res.send(`http://${req.headers.host}/text/${filename}`);
 });
 
-// GET upload txt random
+// GET upload txt random (có thể tạo file mới từ GET)
 app.get('/upload/txt/:content', (req, res) => {
     const content = req.params.content;
     const fileName = `${Date.now()}_${Math.random().toString(36).substring(7)}.txt`;
@@ -96,7 +97,7 @@ app.get('/upload/txt/:content', (req, res) => {
     res.send(`http://${req.headers.host}/txt/${fileName}`);
 });
 
-// POST upload m3u8
+// POST upload m3u8 (vẫn chỉ cho POST)
 app.post('/upload/m3u8', (req, res) => {
     const content = req.body;
     const fileName = `${Date.now()}_${Math.random().toString(36).substring(7)}.m3u8`;
@@ -118,20 +119,7 @@ app.get('/m3u8/:filename', (req, res) => {
     }
 });
 
-// Hàm XÓA FILE bằng GET
-app.get('/del/:filename', (req, res) => {
-    const filename = req.params.filename;
-    const filePath = path.join(UPLOAD_DIR, filename);
-    if (fs.existsSync(filePath)) {
-        fs.unlinkSync(filePath);
-        console.log(`[x] Đã xoá file: ${filename}`);
-        res.send(`Đã xoá file: ${filename}`);
-    } else {
-        res.status(404).send('Không tìm thấy file để xoá ~~');
-    }
-});
-
-// Xoá file tự động quá 12 giờ
+// Xoá file cũ hơn 12 giờ
 setInterval(() => {
     const files = fs.readdirSync(UPLOAD_DIR);
     const now = Date.now();
@@ -143,9 +131,9 @@ setInterval(() => {
             console.log(`[!] Đã xoá file cũ: ${file}`);
         }
     }
-}, 60 * 60 * 1000);
+}, 60 * 60 * 1000); // mỗi 1 giờ
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`[Mahiro UwU] Server chạy tại: http://localhost:${PORT}`);
+    console.log(`[Mahiro UwU] Server chạy ngon lành tại: http://localhost:${PORT}`);
 });
